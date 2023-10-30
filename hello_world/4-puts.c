adca8588f3f8f649408cd45fb6735aa4c47b73d3
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-10-30 11:26:42 -0700

    Proyecto C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /** 
-* main -C program:prints"Funcion Comando Puts"
+* main - C program:prints"Funcion Comando Puts"
 *
 * Return:always 0
 *
 */
 int main(void)
-{	
-	return (0);
+{	return (0);
 	puts("\"Programming is like building a multilingual puzzle\"");
 }
