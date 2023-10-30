d5d1e767985cb62440f884a2bfa10736229caacd
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-10-30 12:44:27 -0700

    Proyecto C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
- /** 
+ /*
  * main - Funcion Comando Puts
  * description 'Uso Funcion Puts'
  * parametro: se usa la funcion main
@@ -9,7 +9,7 @@
  */
 
 int main(void)	
-	{	
+{	
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
