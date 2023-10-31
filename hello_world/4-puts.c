d86b37830f016f903a7b7b84affb7962ad2ead93
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-10-31 14:17:27 -0700

    Funtion printf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
- /*
- * main - C Funcion Comando Puts
- * description 'Uso Funcion Puts'
- * parameter:se usa la funcion main  
- * Return: always 0 (Success)
- */
+/*
+* main - C Funcion Comando Puts
+* description 'Uso Funcion Puts'
+* parameter:se usa la funcion main  
+* Return: always 0 (Success)
+*/
 
 int main(void);
 {	puts("Programming is like building a multilingual puzzle");
