288f4d186460ca31d3addffd9e35c9f91c501a56
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-10-31 12:59:59 -0700

    Funtion printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
   2 
-  3  /**
-  4  * main - Funtion Command Prints a specific line of text
-  5  * description 'Uso Funcion printf'
-  6  * parametro: se usa la funcion main
-  7  *
-  8  * Return: always 0 (Success)
-  9  */
+  3/**
+  4* main - Funtion Command Prints a specific line of text
+  5* description 'Uso Funcion printf'
+  6* parametro: se usa la funcion main
+  7*
+  8* Return: always 0 (Success)
+  9*/
  10 
- 11 int main(void)
+ 11int main(void)
  12 
- 13 {       printf("\"with proper grammar, but the outcome is a piece of art\n");
- 14 return (0);
- 15 }
+ 13{       printf("\"with proper grammar, but the outcome is a piece of art\n");
+ 14return (0);
+ 15}
