6e7a8653e42bea1ab7183d89693d999a7bb5001e
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-10-31 14:00:43 -0700

    Funtion printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
 *main - Funtion Command Prints a specific line of text
-*description'Uso Funcion printf' 
-*
+* description"Uso Funcion printf" 
+
 * Return: always 0 (Success)
 */
 
