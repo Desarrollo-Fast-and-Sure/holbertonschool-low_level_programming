7ffd16e541c2467c760c82a8cb81eda36222faeb
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-03 11:15:53 -0700

    Digit Last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,6 @@ if (last_digit  > 5)
 printf("last digit of %d is %d greater than 5\n", n, last_digit);
 
 else if (last_digit == 0)	
-
 printf("last_digit of %d is %d and is 0\n", n, last_digit);
 else
 printf("last_digit of %d is %d and less than 6 and not 0\n", n, last_digit);
