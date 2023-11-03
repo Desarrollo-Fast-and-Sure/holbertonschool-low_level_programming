12b6b81b337d93ca0757922970ecaf57004ab346
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-03 10:02:09 -0700

    Digit Last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,12 +20,14 @@ int n, last_digit;
 	}
 
 	else if (last_digit  == 0)
-
 	{	
 	printf("last_digit of %d is %d and is 0\n", n, last_digit);
-	}  
+	} 
+
 	else
+
         {	
+
 	printf("last_digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
         }  
 	return (0);
