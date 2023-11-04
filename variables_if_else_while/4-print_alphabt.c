0d678140cd96fae51ab1d451ab3d6d3e318f9d16
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-04 14:07:20 -0700

    Funtion Prints Letters

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+*main - Program that prints the alphabet and the letter 'q' excludes 'e'
+*Return: return (0).
+*/
+int main(void)
+{
+ 
+char letters; /* variable is declared to represent a letter */
+/* Prints lowercase letters from 'a' to 'z' */
+ 
+for (letters = 'a'; letters <= 'z'; letters++)
+{
+    
+/* Conditional which excludes the letters q and e */
+if (letters != 'q' && letters != 'e')
+ 
+{
+
+        putchar(letters);
+}
+ 
+putchar('\n'); /* line break */
+ 
+return (0);
+} 
+
