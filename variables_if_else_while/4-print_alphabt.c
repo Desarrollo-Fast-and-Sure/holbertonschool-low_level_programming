62e0406970b96a2b015e830690a1c1e50186a7cf
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-04 14:13:32 -0700

    Funtion Prints Letters

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,12 @@
 int main(void)
 {
  
-char letters; /* variable is declared to represent a letter */
+char letters;
+/* variable is declared to represent a letter */
 /* Prints lowercase letters from 'a' to 'z' */
- 
 for (letters = 'a'; letters <= 'z'; letters++)
 {    
+
 /* Conditional which excludes the letters q and e */
 if (letters != 'q' && letters != 'e')
  
