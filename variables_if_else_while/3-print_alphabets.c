d043ffe74c5bb4b135f8c89e793425b5df060597
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-04 13:03:31 -0700

    Program Prints Alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Program that prints teh alphabet in lowercase and then in uppercsase
+*Return: return (0).
+*/
+int main(void)
+{
+ 
+char letters; /* variable is declared to represent a letter */
+ 
+         /* Prints lowercase letters from 'a' to 'z' */
+	 for (letters = 'a'; letters <= 'z'; letters++)
+  
+         {
+		 /* conditional which excludes the letters q and e */
+		 if  (letters ! = 'q' && letters ; letters != 'e')
+		 {
+			 putchar(letters);
+               	 }  
+	 }
+	putchar('\n'); /* line break */
+ 
+	return (0);
+} 
