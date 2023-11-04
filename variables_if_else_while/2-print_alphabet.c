684ca683c4a40fa2412067cb39cae21f7ccc1422
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-03 19:40:20 -0700

    FuntionPutchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,17 +5,16 @@
 */
 int main(void);
 
-	{
+{
 	char alphabet;
 
 	alphabet = 'a';
 
-	for (alphabet) = 'a'; (alphabet) <= 'z';
+	for ((alphabet) = 'a'; (alphabet) <= 'z');
 
 	putchar(alphabet);
 	alphabet++;
 
 	putchar('\n');
 	return (0);
-	}		
-
+}		
