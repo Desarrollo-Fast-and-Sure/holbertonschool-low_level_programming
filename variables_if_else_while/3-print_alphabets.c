94e7fc3ed0015b98e73276a62c23a158a817f3e2
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-04 13:08:32 -0700

    Program Prints Alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,17 @@
 int main(void)
 {
  
-char letters; /* variable is declared to represent a letter */
- 
+	char letters; /* variable is declared to represent a letter */
          /* Prints lowercase letters from 'a' to 'z' */
-	 for (letters = 'a'; letters <= 'z'; letters++)
-  
+	 for (letters = 'a'; letters <= 'z'; letters++)  
          {
 		 /* conditional which excludes the letters q and e */
 		 if  (letters ! = 'q' && letters ; letters != 'e')
 		 {
-			 putchar(letters);
+		 putchar(letters);
                	 }  
 	 }
-	putchar('\n'); /* line break */
+	   	 putchar('\n'); /* line break */
  
 	return (0);
 } 
