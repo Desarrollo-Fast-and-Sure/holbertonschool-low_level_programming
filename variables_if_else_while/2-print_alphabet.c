fa7c9f396e708338fc639e8f701977a495408337
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-04 12:19:26 -0700

    Funtion Putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,18 +4,20 @@
 *Return: return (0).
 */
 int main(void)
+{
 
-{  	
 char alphabet;
 
 	alphabet = 'a';
 
 	while (alphabet <= 'z')
-	{	
+	
+	{
 		putchar(alphabet);
 		alphabet++;
 
 	}
+
 	putchar('\n');	
 	return (0);
 }
