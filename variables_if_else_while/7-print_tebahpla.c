d080c5c71104fc08f3fe85a6f8b69793ed3787dd
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-05 18:50:37 -0800

    Funtion Putchar

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main - Program that prints the reverse alphabet
+*Return: return (0).
+*/
+int main(void)
+{
+char letters;
+/* Variable is declared to represent a letter */
+/* Prints lowercase letters from 'z' to 'a' */
+for (letters = 'z'; letters >= 'a' ; letters==)
+{
+putchar(letters);
+}
+putchar('\n');
+return (0);
+}
+
