80843f2fdb35efb7f32d7d4bc96c339a2495de2c
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-05 18:14:09 -0800

    Funtion putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+*main - Prints a string with single digit numbers
+*Return: return (0).
+*/
+int main(void)
+{
+int n = 0;
+/* loop to print the digits from 0 to 9 */
+/* Every time n is less than 10 it will continue to iterate */
+while (n < 10)
+{
+ 
+putchar("%d", n);
+n++;
+}
+putchar("\n");
+return (0);
+}
+
