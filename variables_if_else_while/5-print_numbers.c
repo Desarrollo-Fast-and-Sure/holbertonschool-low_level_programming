328483c26b8297fe2c310d4f79c2a6717bcd21e1
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-05 17:57:40 -0800

    Funcion Main

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,11 @@ int n = 0;
 /* loop to print the digits from 0 to 9 */
 /* Every time n is less than 10 it will continue to iterate */
 while (n < 10)
-	}
+{
 
-printf("%di", n);
+printf("%d", n);
 	n++;
-	{
+}	
 printf("\n");
 return (0);
 }
