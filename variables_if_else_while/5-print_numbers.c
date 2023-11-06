cfb23bfa2ed5853839f67cbbc8b5d2b4831f7c6c
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-05 17:49:47 -0800

    Funcion Main

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *main - Prints a string with single digit numbers
+*Return: return (0).
+*/
+int main(void)
+{
+int n = 0;
+/* loop to print the digits from 0 to 9 */
+/* Every time n is less than 10 it will continue to iterate */
+while (n < 10)
+{
+
+printf("%di", n)
+	n++;
+{
+printf("\n");
+return (0);
+}
+
