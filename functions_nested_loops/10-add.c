1eee994af427da0c99be3d0540bf186fc0eb13c3
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-07 12:47:03 -0800

    function print

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -6,7 +6,8 @@
  *@n2: Integer variable declaration
  :Return: Result of the sum of n1 and n2
  */
-int add(int n1, int n2);
+int add(int n1, int n2)
 {
-	return (n1 + n2);
+return (n1 + n2);
+return (add);
 }
