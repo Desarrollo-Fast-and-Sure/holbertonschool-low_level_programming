17bba1f2d940bc0e320e0bd8177f3ed54275dd28
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-07 19:27:41 -0800

    function print

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,6 +8,7 @@
 void times_table(void)
 {
 	int n1, n2, result;
+
 	for (n1 = 0; n1 < 10; n1++)
 	{
 		for (n2 = 0; n2 < 10; n2++)
@@ -20,7 +21,7 @@ void times_table(void)
 				printf("%2d, ", result);
 				if (n2 != 9)
 					printf(", ");
-			}		
+			}
 		}
 	printf("\n");
 	}
