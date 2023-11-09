d09b6204743d32750acf2e3a0e127aa43fae11b6
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-09 12:07:36 -0800

    function that takes a pointer

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 
 /**
- *reset_to_98 - Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ *reset_to_98 - Write a function that takes a pointer to an int as parameter *and updates the value it points to to 98.
  * @c: input
  * Return: n
  */
