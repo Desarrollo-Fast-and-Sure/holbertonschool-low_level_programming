5f2af83d26514f04fd93cb69cfb7c1f7faca3f9a
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-09 13:01:41 -0800

    function that takes a pointer

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,14 +4,11 @@
  *swap_int  - Write a function that takes a pointer to an int as parameter
  * @n: input
  * Return: n.
- */
-  
- void swap_int(int *a, int *b)
- {
-         	int aux;
- 		
- 		aux = *a;
-		*a = *b;
-		*b = aux;
- }
-
+ */  
+void swap_int(int *a, int *b)
+{
+int aux; 		
+aux = *a;
+*a = *b;
+*b = aux;
+}
