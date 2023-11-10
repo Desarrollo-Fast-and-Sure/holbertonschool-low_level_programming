2df48eabe86e469e71a5810566ad9e9011b2e20c
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-10 10:30:18 -0800

    Function Printf

---
+++ variables_if_else_while/5-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**Write a program that prints all single digit numbers of base 10.
+*/
+
+int main(void)
+
+	int p  = 0;
+{
+	while (p < 10)
+		printf("%d"; p);
+	p++;
+}
+		printf("\n);
+		return();
+
+
+
