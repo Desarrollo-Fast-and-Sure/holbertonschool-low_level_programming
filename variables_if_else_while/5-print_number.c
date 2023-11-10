093c8f8576ed1ab593d9c6db3768a5e2c3312279
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-10 11:19:41 -0800

    Function Print

--- variables_if_else_while/5-print_number.c
+++ variables_if_else_while/5-print_number.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/**Write a program that prints all single digit numbers of base 10.
+/** main - Write a program that prints all single digit numbers of base 10.
  *Return: Always 0
  */
 
 int main(void)
-
-	int p = 0;
 {
-	while (p < 10)
+	int p = 0;
+
+	while(p < 10)
 	{
 		printf("%d", p);
 	p++;
