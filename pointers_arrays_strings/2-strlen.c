0aee174ab53db551e17887740c98851cd49f9cae
Desarrolo-Fast-And-Sure <gsaavedra7307@gmail.com>
2023-11-13 21:11:41 -0800

    Function Returns

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,12 @@
-#include "main.h"
- 
+#include "main.h" 
 /**
  *_strlen - Write a function that return the length of a strinng
  * @s: char type variable
  * Return: Returns the length of a string (int)
  */
- int _strlen(char *s)
- {
- int i = 0;
-
- for (i = 0; s[i] != '\0'; i++);
-
+int _strlen(char *s)
+{
+int i = 0;
+for (i = 0; s[i] != '\0'; i++);
 return (i);
- }
-
+}
